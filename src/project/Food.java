ff9f514dee244dad4553ffd3d652568639aac3b3
April Bagnani <aprilbagnani@Aprils-MacBook-Pro.local>
2021-03-28 17:27:00 -0500

    food object created

--- src/project/Food.java
+++ src/project/Food.java
@@ -2,4 +2,25 @@ package project;
 
 public class Food {
 
+	private String name;
+    private double price;
+    private int quantity;
+
+    public Food(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
