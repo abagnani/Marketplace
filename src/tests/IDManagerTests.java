6387fe9c93064b5373b2a4462c10fcfbb49cff3a
Nick Cione <nickcione@wustl.edu>
2021-04-13 20:14:56 -0500

    buyers can view current posts

--- src/tests/IDManagerTests.java
+++ src/tests/IDManagerTests.java
@@ -3,6 +3,8 @@ package tests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -41,6 +43,21 @@ class IDManagerTests {
 		System.out.println(isUnique1);
 		assertTrue(isUnique == true);
 		assertTrue(isUnique1 == false);
+	}
+	
+	@Test
+	void testGetIDs() {
+		ItemManager itemManagerBob = new ItemManager("Bob");
+		ItemManager itemManagerJoe = new ItemManager("Joe");
+		IDManager accountsManager = new IDManager();
+		accountsManager.addNewUser(itemManagerBob);
+		accountsManager.addNewUser(itemManagerJoe);
+		
+		Set<String> IDs = accountsManager.getIDs();
+		Set<String> correctSet = new HashSet<String>();
+		correctSet.add("Bob");
+		correctSet.add("Joe");
+		assertEquals(IDs, correctSet);
 	}
 
 }
